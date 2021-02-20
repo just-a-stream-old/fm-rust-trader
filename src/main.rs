89d5f4b536d583877b18780e4ea7b303acce8bd1
keirbailey <keirinvestment@gmail.com>
2021-02-20 16:01:43 +0000

    removed run() method and went with vanilla main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 mod config;
+mod model;
+mod data;
 
 use crate::config::*;
+use crate::model::SymbolData;
+use std::intrinsics::fabsf32;
 
 fn main() {
+    // Load environment configuration
     let config = load_config();
 
-    println!("{:#?}", config);
 
 }
