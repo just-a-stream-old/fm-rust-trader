5a4d1bc4afed5072a25c35856788ddf80e0185a7
keirbailey <keirinvestment@gmail.com>
2021-02-21 13:11:01 +0000

    Added ability to load symbol data from csv with serde and csv crates

--- src/data.rs
+++ src/data.rs
@@ -1,4 +1,6 @@
 use crate::model::{SymbolData, Bar, MarketEvent, EVENT_TYPE_MARKET};
+use crate::config::{Config, Trader};
+use std::error::Error;
 
 const DATA_DIRECTORY: &str = "resources/data/";
 
@@ -7,20 +9,35 @@ pub struct HistoricDataHandler {
     symbol:                 String,
     all_symbol_data:        SymbolData,
     current_symbol_data:    SymbolData,
-    latest_bar_index:       i64
+    latest_bar_index:       usize
 }
 
 impl HistoricDataHandler {
+    fn new(cfg: Trader, event_q: Vec<MarketEvent>) -> HistoricDataHandler {
+        // Todo: log debug file path
+        // Todo: Improve error handling, pass back to main with added info like Go? errors.wrap()
+        let all_symbol_data = match load_csv_symbol_data(cfg) {
+            Ok(data) => data,
+            Err(err) => panic!("{}", err)
+        };
+
+
+
+
+    }
+
     fn should_continue(&self) -> bool {
-        // Return true if the latest_bar_index has not reached the end of the data
-        self.latest_bar_index < ((self.all_symbol_data.timestamps.len() as i64) - 1)
+        // Return true if the next latest_bar_index has Some data instead of None
+        self.all_symbol_data.timestamps
+            .get(self.latest_bar_index + 1)
+            .is_some()
     }
 
     fn update_data(&mut self) {
         // Increment latest_bar_index
         self.latest_bar_index += 1;
 
-        // Add latest bar to current_symbol_data
+        // Add latest bar to current_symbol_data (index checked in should_continue so is safe)
         let latest_bar = Bar{
             timestamp: self.all_symbol_data.timestamps[self.latest_bar_index],
             open: self.all_symbol_data.opens[self.latest_bar_index],
@@ -29,15 +46,42 @@ impl HistoricDataHandler {
             close: self.all_symbol_data.closes[self.latest_bar_index],
             volume: self.all_symbol_data.volumes[self.latest_bar_index],
         };
-        self.current_symbol_data.add_bar(latest_bar);
+        self.current_symbol_data.add_bar(&latest_bar);
 
         // Add MarketEvent to the queue
         self.event_q.push(MarketEvent{
             event_type: EVENT_TYPE_MARKET,
             trace_id: String::from("add uuid here"),
             timestamp: latest_bar.timestamp,
-            symbol: self.symbol,
+            symbol: self.symbol.clone(),
             close: latest_bar.close,
         });
     }
+
+    fn get_latest_data(&self) -> (&SymbolData, usize) {
+        (&self.current_symbol_data, self.latest_bar_index)
+    }
+}
+
+// Todo: Move func to lib?
+fn load_csv_symbol_data(&cfg: Trader) -> Result<SymbolData, csv::Error> {
+    // Read the file
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(true)
+        .delimiter(b',')
+        .from_path(build_csv_file_path(cfg))?;
+
+    // Iterate through the lines & add the deserialised Bar struct to symbol_data
+    let mut symbol_data = SymbolData::default();
+
+    for result_bar in reader.deserialize() {
+        symbol_data.add_bar(&result_bar?);
+    }
+
+    return Ok(symbol_data)
+}
+
+// Todo: Move func to lib?
+fn build_csv_file_path(&cfg: Trader) -> String {
+    format!("{}{}_{}.csv", DATA_DIRECTORY, cfg.symbol, cfg.timeframe)
 }
