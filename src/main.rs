484cbb3c46ccbea768ffbead02514a14ca0e6672
keirbailey <keirinvestment@gmail.com>
2021-02-21 13:11:52 +0000

    Added serde derivations & default() method for SymbolData

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,18 @@ mod model;
 mod data;
 
 use crate::config::*;
-use crate::model::SymbolData;
-use std::intrinsics::fabsf32;
+use crate::model::{Bar, SymbolData};
 
 fn main() {
+    // if let Err(err) = run() {}
+
     // Load environment configuration
     let config = load_config();
 
+    let symbol_data = load_csv_symbol_data(
+        String::from("resources/data/ETH-USD_1D.csv")
+    ).unwrap();
+
+    println!("{:?}", symbol_data);
 
 }
--- src/model.rs
+++ src/model.rs
@@ -1,32 +1,40 @@
-use chrono::{DateTime, Utc};
+use chrono::{NaiveDate};
 use std::collections::HashMap;
+use serde::{Deserialize};
 
-pub const EVENT_TYPE_MARKET: String = String::from("MARKET_EVENT");
-const EVENT_TYPE_SIGNAL: String = String::from("SIGNAL_EVENT");
-const EVENT_TYPE_ORDER: String = String::from("ORDER_EVENT");
-const EVENT_TYPE_FILL: String = String::from("FILL_EVENT");
+pub const EVENT_TYPE_MARKET: &str = "MARKET_EVENT";
+const EVENT_TYPE_SIGNAL: &str = "SIGNAL_EVENT";
+const EVENT_TYPE_ORDER: &str = "ORDER_EVENT";
+const EVENT_TYPE_FILL: &str = "FILL_EVENT";
 
 pub struct MarketEvent {
-    pub event_type: String,
+    pub event_type: &'static str,
     pub trace_id: String,
-    pub timestamp: DateTime<Utc>,
+    pub timestamp: NaiveDate,
     pub symbol: String,
     pub close: f64,
 }
 
 // Bar represents a symbol's market data state at a fixed interval of time
+#[derive(Debug, Deserialize)]
 pub struct Bar {
-    pub timestamp: DateTime<Utc>,
+    #[serde(rename = "Date")]
+    pub timestamp: NaiveDate,
+    #[serde(rename = "Open")]
     pub open: f64,
+    #[serde(rename = "High")]
     pub high: f64,
+    #[serde(rename = "Low")]
     pub low: f64,
+    #[serde(rename = "Adj Close")]
     pub close: f64,
     pub volume: f64,
 }
 
 // SymbolData represents a symbol's struct of market data arrays (OHLCV) and associated indicators values
+#[derive(Debug)]
 pub struct SymbolData {
-    pub timestamps: Vec<DateTime<Utc>>,
+    pub timestamps: Vec<NaiveDate>,
     pub opens: Vec<f64>,
     pub highs: Vec<f64>,
     pub lows: Vec<f64>,
@@ -35,8 +43,16 @@ pub struct SymbolData {
     pub indicators: HashMap<String, Vec<f64>>,
 }
 
+impl Default for SymbolData {
+    fn default() -> SymbolData {
+        SymbolData{
+            timestamps: vec![], opens: vec![], highs: vec![], lows: vec![],
+            closes: vec![], volumes: vec![], indicators: Default::default()}
+    }
+}
+
 impl SymbolData {
-    pub fn add_bar(&mut self, bar: Bar) -> &mut SymbolData {
+    pub fn add_bar(&mut self, bar: &Bar) -> &mut SymbolData {
         self.timestamps.push(bar.timestamp);
         self.opens.push(bar.open);
         self.highs.push(bar.high);
